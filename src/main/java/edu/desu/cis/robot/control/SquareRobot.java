bc3d92a1242d405cdd55b7ab1bc86bdc3fc70861
Marwan Rasamny <mrasamny@desu.edu>
2026-04-17 10:46:54 -0400

    first commit

---
+++ src/main/java/edu/desu/cis/robot/control/SquareRobot.java
@@ -0,0 +1,21 @@
+package edu.desu.cis.robot.control;
+
+import edu.desu.cis.robot.control.RobotController;
+
+public class SquareRobot extends RobotController {
+    public SquareRobot(String robotName) {
+        super(robotName);
+    }
+    @Override
+    public void run() {
+        for(int count = 0; count < 4; count++){
+            mbot.straight(30);
+            mbot.turnRight(90);
+        }
+    }
+    public static void main(String[] args) {
+        try (SquareRobot robot = new SquareRobot("StingBot")) {
+            robot.run();
+        }
+    }
+}
